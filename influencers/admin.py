473cbe5673dacc477cd429edaef3bd5799934446
Crazychukz <chukwuemekacharles88@gmail.com>
2016-12-12 15:38:44 +0100

    new2nkkb

--- influencers/admin.py
+++ influencers/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 from .models import *
+from .models import Campaigns
 
 
 # Register your models here.
+class CampaignsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'action', 'base_pay' , 'dummy_tracker', 'approved')
 
-admin.site.register(NoisemakerProfile)
-admin.site.register(Campaigns)
-admin.site.register(Tracker)
-admin.site.register(Payouts)
-admin.site.register(Requested)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'decibel', 'number_of_friends', 'rank', 'escrow')
+class TrackerAdmin(admin.ModelAdmin):
+    list_display = ('trackers_ID', 'tracking_ID', 'campaign')
+
+class PayoutAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount_requested','date_requested', 'approved', 'paid' )
+
+admin.site.register(NoisemakerProfile, ProfileAdmin)
+admin.site.register(Campaigns, CampaignsAdmin)
+admin.site.register(Tracker, TrackerAdmin)
+admin.site.register(Payouts, PayoutAdmin)
+admin.site.register(Requested,)
+
+
+
+
--- influencers/models.py
+++ influencers/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 import datetime
+from django.utils.encoding import python_2_unicode_compatible
 from django.db.models.fields import IntegerField
 
 
@@ -37,20 +39,25 @@ class NoisemakerProfile(models.Model):
         ('All', 'All'),
 
     )
-    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True,)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,verbose_name=('username'), primary_key=True,)
     twitter_handle = models.CharField(max_length=250, null=True)
     facebook_handle = models.CharField(max_length=250, null=True)
     instagram_handle = models.CharField(max_length=250, null=True)
     twitter_ID = models.BigIntegerField(null=False,  default=0)
     facebook_ID = models.IntegerField(null=False, default=0)
     instagram_ID = models.BigIntegerField(null=False, default=0)
-    number_of_friends = models.IntegerField(null=False, default=0)
-    escrow = models.FloatField(null=False, default=0.0)
-    decibel = models.FloatField(null=False, default=0.5)
+    number_of_friends = models.IntegerField(null=False,verbose_name=('number of friends'), default=0)
+    escrow = models.FloatField(null=False,verbose_name=('account'), default=0.0)
+    decibel = models.FloatField(null=False,verbose_name=('decibel'), default=0.5)
     account_number = models.IntegerField(null=False, default=0)
     account_name = models.CharField(max_length=250, null=True)
     bank_name = models.CharField(max_length=250, choices=BANKS, null=True)
     preferences = models.CharField(max_length=250, choices=PREFERENCES, null=True)
+
+    class Meta:
+        ordering = ["pk"]
+        verbose_name = 'Noisemakerz Profile'
+
 
     def __str__(self):
         return '%s %s %s %s' % (self.user, self.twitter_handle, self.decibel, self.escrow)
@@ -70,6 +77,7 @@ class NoisemakerProfile(models.Model):
 
 
 
+@python_2_unicode_compatible
 class Campaigns(models.Model):
     ACTIONS = (
         ('Retweet', 'Retweet'),
@@ -87,23 +95,23 @@ class Campaigns(models.Model):
 
     )
     campaign_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    base_pay = models.IntegerField(null=False, default=0)
+    user = models.ForeignKey(User,verbose_name=('user'), on_delete=models.CASCADE)
+    base_pay = models.IntegerField(null=False,verbose_name=('base pay'), default=0)
     url = models.CharField(max_length=250, blank=True, null=True, default='')
-    action = models.CharField(max_length=100, choices= ACTIONS)
+    action = models.CharField(max_length=100, verbose_name=('action'), choices= ACTIONS)
     tracking_ID = models.CharField(max_length=500, null=True, blank=True)
     tweet = models.CharField(max_length=200, null=True, blank=True, default='')
     follow_handle = models.CharField(max_length=200, blank=True,null=True, default='')
     follow_handle_id = models.CharField(max_length=250, null=True, blank=True)
     hash_tag = models.CharField(max_length=50, blank=True,null=True, default='')
-    dummy_tracker = models.CharField(max_length=300, null=True)
+    dummy_tracker = models.CharField(max_length=300, verbose_name=('tracking number'), null=True)
     activity_count = models.IntegerField(null=False, default=0)
     estimated_reach = models.IntegerField(null=False, default=0)
     decibel = models.FloatField(null=False, default=0.5)
     preferences = models.CharField(max_length=250, choices=PREFERENCES)
-    budget = models.FloatField(null=False, default=0.0)
+    budget = models.FloatField(null=False, verbose_name=('budget'), default=0.0)
     time_created = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
+    approved = models.BooleanField(verbose_name=('approved'),default=False)
     featured = models.BooleanField(default=False)
 
     def __str__(self):
@@ -111,12 +119,13 @@ class Campaigns(models.Model):
 
     class Meta:
         ordering = ["campaign_id"]
-        db_table = "Campaign"
+        verbose_name = 'Campaign'
+        db_table = 'Campaign'
 
 class  Tracker(models.Model):
-    tracking_ID = models.CharField(max_length=500)
-    campaign = models.ForeignKey(Campaigns, on_delete=models.CASCADE)
-    trackers_ID = models.ForeignKey(User, on_delete=models.CASCADE)
+    tracking_ID = models.CharField(verbose_name=('tracking id'),max_length=500)
+    campaign = models.ForeignKey(Campaigns,verbose_name=('campaign'), on_delete=models.CASCADE)
+    trackers_ID = models.ForeignKey(User,verbose_name=('user'), on_delete=models.CASCADE)
     action_count = models.CharField(max_length=10000)
     tracked = models.BooleanField()
 
@@ -130,17 +139,18 @@ class  Tracker(models.Model):
 
     class Meta:
         db_table = "Activities"
+        verbose_name = 'Tracker'
 
 
 
 class Payouts(models.Model):
     payout_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    amount_requested = models.FloatField(null=False, default=0.0)
-    approved = models.BooleanField()
-    note = models.TextField()
-    paid = models.BooleanField()
-    date_requested = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,verbose_name=('user'), on_delete=models.CASCADE)
+    amount_requested = models.FloatField(null=False,verbose_name=('amount requested'), default=0.0)
+    approved = models.BooleanField(verbose_name=('approved'))
+    note = models.TextField(verbose_name=('note'),)
+    paid = models.BooleanField(verbose_name=('paid'),)
+    date_requested = models.DateTimeField(auto_now_add=True,verbose_name=('date requested'),)
 
     def __str__(self):
      return '%s %s ' % (self.user, self.amount_requested)
@@ -149,8 +159,17 @@ class Payouts(models.Model):
     class Meta:
         ordering =["payout_id"]
         db_table = "Influencers Payment"
+        verbose_name = 'Noisemakerz Payout'
 
 
 class Requested(models.Model):
     requested = models.EmailField(unique=True, verbose_name=('e-mail address'),max_length=254)
+
+    def __str__(self):
+     return '%s  ' % (self.requested,)
+
+    class Meta:
+
+        verbose_name = 'Request'
+
 
