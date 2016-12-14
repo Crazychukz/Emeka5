5b8f101a0be0c705a140836e44228dbea61d4927
Crazychukz <chukwuemekacharles88@gmail.com>
2016-12-14 18:42:59 +0100

    additional files and changes for Heroku

--- Noisemakers/settings.py
+++ Noisemakers/settings.py
@@ -148,11 +148,11 @@ DATABASES['default'].update(db_from_env)
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-STATIC_ROOT= 'http://stormy-coast-67103.herokuapp.com/influencers/static/influencers/'
+STATIC_ROOT= '/static/influencers/'
 SITE_ID = 1
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/static/influencers/',
+    '/influencers/static/influencers/',
 ]
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
