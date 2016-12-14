3dc2134612398856c830d74dc69de806069c76d3
Crazychukz <chukwuemekacharles88@gmail.com>
2016-12-14 23:03:35 +0100

    additional files and changes for Heroku

--- Noisemakers/settings.py
+++ Noisemakers/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 ADMIN_MEDIA_PREFIX = '/static/admin/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
